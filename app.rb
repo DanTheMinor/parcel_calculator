a12ba8ee953700494007bd47396833ef14453fae
Dan Minor & Adam McFadden <student@epicodus.com>
2015-04-27 15:14:47 -0700

    pass the integration spec, and perfor as requested. Taking inputs for parcel parameters and outputting the cost to ship.

---
+++ app.rb
@@ -0,0 +1,20 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/parcels')
+also_reload('lib/**/*.rb')
+
+get ('/') do
+  erb(:index)
+end
+
+get('/parcels') do
+  @length = params.fetch('length').to_i()
+  @width = params.fetch('width').to_i()
+  @height = params.fetch('height').to_i()
+  @weight = params.fetch('weight').to_i()
+  @distance = params.fetch('distance').to_i()
+  @ship_speed = params.fetch('ship_speed')
+  test_parc = Parcels.new(@length, @width, @height, @weight)
+  @cost = test_parc.cost_to_ship(@distance, @ship_speed)
+  erb(:parcel)
+end
--- spec/parcels_integration_spec.rb
+++ spec/parcels_integration_spec.rb
@@ -2,8 +2,9 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+require 'pry'
 
-describe('The parsels path') do
+describe('The parsels path', {:type => :feature}) do
   it('take input through forms, send them through the cost_to_ship method, return the cost of shipping to the user.') do
     visit('/')
     fill_in('length', :with => 5)
@@ -11,7 +12,7 @@ describe('The parsels path') do
     fill_in('height', :with => 5)
     fill_in('weight', :with => 20)
     fill_in('distance', :with => 500)
-    #select ship_speed from dropdown
+    find_field('ship_speed').find('option[1]').select_option #Finds an id for a select html block. Then finds the option counting from the top down (starting at 1). Select_option selects that option.
     click_button('calculate_cost')
     expect(page).to have_content('$12.00')
   end
